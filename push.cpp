2dda9658eb4ef96115c2995a2f50ab026ac50f9b
Misha777-eng <69074969+Misha777-eng@users.noreply.github.com>
2021-06-28 22:53:44 +0300

    Add files via upload

---
+++ push.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+cout << "ПУШ НАЧАЛСЯ !!!!!!11111!!!!!!!!1111!!!1111!!!111!!!!11111!!!!!1111!!!!\n";
+while(1)
+{
+cout << "!УВАГА! !WARNING! !あはま!УРОВЕНЬ СЛИТИЯ ПОДНЯТ ДО 666%DOLMANOV LEVEL PUSHED TO 666%アサタカアマカヤあさはたなまさ666%";
+}
+}
